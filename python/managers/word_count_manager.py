fec886e072cbf6d50e6df61a875c04a8513b8ff5
Dmitry Gorbunov <dgorbunov@aimtech.com>
2017-04-15 21:57:29 +0300

    Commit

--- python/managers/word_count_manager.py
+++ python/managers/word_count_manager.py
@@ -12,20 +12,20 @@ class WordCountManager:
         if not os.path.exists(config["parameters"]["word_count"]["path"] + "/result.txt"):
             self.calculate_word_count()
         self.count = {}
-        for line in filter(lambda line: len(line) > 0, map(lambda line: line.split(), open(config["parameters"]["word_count"]["path"] + "/result.txt").read().split("\n"))):
+        for line in filter(lambda line: len(line) > 0, map(lambda line: line.split(), open(config["parameters"]["word_count"]["path"] + "/result.txt").read().decode('utf-8').split("\n"))):
             self.count[line[0]] = int(line[1])
 
     @staticmethod
     def calculate_word_count():
         count = defaultdict(int)
-        tokens = open(config["parameters"]["raw_model"]["path"]).read().decode("utf-8").split()
+        tokens = open(config["parameters"]["raw_model"]["path"]).read().split()
         ticker = Ticker(logger, len(tokens), "calculate_word_count")
         for token in tokens:
             count[token] += 1
             ticker()
         fout = open(config["parameters"]["word_count"]["path"] + "/result.txt", "w")
         for entity in count.iteritems():
-            fout.write("%s %d\n" % entity)
+            fout.write(u"%s %d\n" % entity)
         fout.close()
 
     def check_word_counts(self, word2vec, word1, word2):
