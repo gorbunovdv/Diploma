8e9ba988aa8c497fd90239a27484a406130727a8
Dmitry Gorbunov <dgorbunov@aimtech.com>
2017-04-15 22:05:10 +0300

    Test

--- python/managers/word_count_manager.py
+++ python/managers/word_count_manager.py
@@ -1,6 +1,7 @@
 import os
 from collections import defaultdict
 from itertools import imap
+import codecs
 
 from python.config.config import config
 from python.logger.logger import Ticker, Logger
@@ -12,20 +13,23 @@ class WordCountManager:
         if not os.path.exists(config["parameters"]["word_count"]["path"] + "/result.txt"):
             self.calculate_word_count()
         self.count = {}
-        for line in filter(lambda line: len(line) > 0, map(lambda line: line.split(), open(config["parameters"]["word_count"]["path"] + "/result.txt").read().decode('utf-8').split("\n"))):
+        for line in filter(lambda line: len(line) > 0, map(lambda line: line.split(), codecs.open(config["parameters"]["word_count"]["path"] + "/result.txt", 'r', 'utf-8').read().split("\n"))):
             self.count[line[0]] = int(line[1])
 
     @staticmethod
     def calculate_word_count():
         count = defaultdict(int)
-        tokens = open(config["parameters"]["raw_model"]["path"]).read().decode('utf-8').split()
+        tokens = codecs.open(config["parameters"]["raw_model"]["path"], 'r', 'utf-8').read().split()
         ticker = Ticker(logger, len(tokens), "calculate_word_count")
         for token in tokens:
             count[token] += 1
             ticker()
-        fout = open(config["parameters"]["word_count"]["path"] + "/result.txt", "w")
-        for entity in count.iteritems():
-            fout.write(u"%s %d\n" % entity)
+        fout = codecs.open(config["parameters"]["word_count"]["path"] + "/result.txt", "w", 'utf-8')
+        for word, cnt in count.iteritems():
+            print type(word)
+            fout.write(word)
+            fout.write(' ')
+            fout.write(str(cnt))
         fout.close()
 
     def check_word_counts(self, word2vec, word1, word2):
