4d122e1051a176571e057e31df5ee9e2c0cfa278
Dmitry Gorbunov <gorbunovdv@gmail.com>
2017-04-24 11:18:33 +0300

    sh script fixed

--- amazon.sh
+++ amazon.sh
@@ -9,5 +9,4 @@ pip install numpy pathos gensim
 git clone https://github.com/open-source-parsers/jsoncpp.git
 cd jsoncpp && cmake . && make -j4 && sudo make install && cd ..
 mkdir build && cd build && cmake .. && make && cd ..
-mkdir -p results/{filtered,sorted,sorted_each,transformations_build}
-wget https://s3-eu-west-1.amazonaws.com/dsl-research/wiki/w2v_export/en/corpus_en.norm-sz100-w8-cb0-it1-min20.w2v
+mkdir -p results/{filtered,sorted,sorted_each,transformations_build,nearest_neighbours,acyclic_graph}
