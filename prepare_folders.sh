9a0687d569d37f95f8352733283efa9ae0c896bb
Ubuntu <ubuntu@ip-172-31-67-241.ec2.internal>
2017-05-13 13:13:45 +0000

    Fixed bug in prepare_folders.sh

--- prepare_folders.sh
+++ prepare_folders.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-mkdir -p results/{filtered,sorted,sorted_each,transformations_build,nearest_neighbours,acyclic_graph}
+mkdir -p results/{filtered,sorted,sorted_each,transformations_build,nearest_neighbours,acyclic_graph,morphological_transformations,evaluation}
